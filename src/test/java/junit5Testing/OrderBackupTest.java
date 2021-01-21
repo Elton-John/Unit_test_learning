c1dd8021449c174e366c4b8356ba48e4434b4b7b
Olga <nauka.dinosaurus@gmail.com>
2021-01-21 07:48:05 +0100

    test @BeforeAll and @AfterAll

---
+++ src/test/java/junit5Testing/OrderBackupTest.java
@@ -0,0 +1,40 @@
+package junit5Testing;
+
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class OrderBackupTest {
+
+    private static OrderBackup orderBackup;
+
+    @BeforeAll
+    static void setup() throws FileNotFoundException {
+        orderBackup = new OrderBackup();
+        orderBackup.createFile();
+    }
+
+    @Test
+    public void shouldAppendOrderToFile() throws IOException {
+        //given
+        Order order = new Order();
+        Meal meal = new Meal(10, "burger");
+        order.addMealToOrder(meal);
+
+        //when
+        orderBackup.backupOrder(order);
+
+        //then
+        System.out.println(order.toString() + "backed up");
+    }
+
+    @AfterAll
+    static void tearDown() throws IOException {
+        orderBackup.closeFile();
+    }
+
+}
